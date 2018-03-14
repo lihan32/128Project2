e8ac0201c070d5d0e86aa7425a5794037288f5d3
lihan32 <35952835+lihan32@users.noreply.github.com>
2018-03-14 03:47:02 -0700

    Add files via upload

--- Network.m
+++ Network.m
@@ -10,11 +10,9 @@ function [ output ] = Network( n,Train,nlayer, nneuron,W)
     output={};
     input = Read(Train,n,0);        %input value is nth row of train matrix
     [~,m]=size(input);                %set m be the size of input
-    for i=1:nlayer-1
+    for i=1:nlayer
         input=Layer(input,nneuron,W{1,i}); %to other layers, we need to use the weight matrix W
         output=[output,input];
     end
-    input=Layer(input,m,W{1,nlayer});%last output layer.
-    output=[output,input]; % the last input is the output(input to the output layer)
 end
 
--- Workshop.m
+++ Workshop.m
@@ -1,32 +1,65 @@
 %this script will use all function we define to train our network.
 %all parameters and input will be set up here.
 
-nneuron=784;
-nlayer=5;
+nneuron=784;%num of neuron in a layer
+nlayer=18;%num of layer
+%generate a random matrix
+W={0.001*rand(784,784)-0.005,0.001*rand(784,784)-0.005,0.001*rand(784,784)-0.005,0.001*rand(784,784)-0.005,0.001*rand(784,784)-0.005,0.001*rand(784,784)-0.005,0.001*rand(784,784)-0.005,0.001*rand(784,784)-0.005,0.001*rand(784,784)-0.005,0.001*rand(784,784)-0.005,0.001*rand(784,784)-0.005,0.001*rand(784,784)-0.005,0.001*rand(784,784)-0.005,0.001*rand(784,784)-0.005,0.001*rand(784,784)-0.005,0.001*rand(784,784)-0.005,0.001*rand(784,784)-0.005,0.001*rand(784,784)-0.005};
+err=zeros(980,1);%measure the norm of error bewteen result and target
 
-W={0.001*rand(784,784),0.001*rand(784),0.001*rand(784),0.001*rand(784,784),0.001*rand(784,784)};
-err=zeros(980,1);
-for i=1:98
-output=Network(i,(train1/256),nlayer,nneuron,W);
-error=((double(test1(i,:))/255)-output{nlayer}');
+%reverse propagation here
+for i=1:198%determine the number of training pairs here
+output=Network(i,(double(train1)/256),nlayer,nneuron,W);%restore output
+error=abs((double(test1(i,:))/256)-output{nlayer}');%calculate error
 err(i)=norm(error);
 delta={};
-rate = 0.05;
-for j=1:nlayer
-    if j==1
-        delta=[delta,(output{nlayer}.*(1-output{nlayer}))'.*error];
-        W{nlayer}=W{nlayer}+rate*(output{nlayer}.*((1-output{nlayer})'.*error)'*output{nlayer-1}');
-    else
-    if j~=nlayer
-        delta=[delta,delta{j-1}*W{nlayer-j+1}'.*(output{nlayer-j+1}'.*(1-output{nlayer-j+1}'))];
-        a=rate*delta{j}'*output{nlayer-j}';
-        W{nlayer-j+1}=W{nlayer-j+1}+a;
-    else
-        delta=[delta,delta{j-1}*W{nlayer-j+1}'.*(output{nlayer-j+1}'.*(1-output{nlayer-j+1}'))];
-        a=rate*delta{j}'*double(train0(i,:));
-        W{nlayer-j+1}=W{nlayer-j+1}+a;
-    end
+rate = 0.05;%this is the arbitrary in the document
+
+
+
+for CurrentLayer=nlayer:1
+    for CurrentLayer=nlayer:-1:1
+        if CurrentLayer==nlayer
+            for p=1:784
+                Tdelta=zeros(784,1);%delta for current layer
+                %this loop construct the delta for current layer
+                for q=1:784
+                    o=output{CurrentLayer}(q);
+                    Tdelta(q)=o*(1-o)*error(q);
+                end
+                %calculate w(n+1) here
+                W{CurrentLayer}(:,p)=W{CurrentLayer}(:,p)+rate*Tdelta*output{CurrentLayer-1}(p);
+            end
+            %restore delta of this layer
+            delta=[delta,Tdelta];
+        else
+            if CurrentLayer~=1
+                for p=1:784
+                    Tdelta=zeros(784,1);
+                    for q=1:784
+                        o=output{CurrentLayer}(q);
+                        %the formula of delta is diff from the output layer
+                        Tdelta(q)=o*(1-o)*delta{nlayer-CurrentLayer}'*W{CurrentLayer+1}(:,p);
+                    end
+                    W{CurrentLayer}(:,p)=W{CurrentLayer}(:,p)+rate*Tdelta*output{CurrentLayer-1}(p);
+                end
+                delta=[delta,Tdelta];
+            else
+                for p=1:784
+                    Tdelta=zeros(784,1);
+                    for q=1:784
+                        o=output{CurrentLayer}(q);
+                        Tdelta(q)=o*(1-o)*delta{nlayer-CurrentLayer}'*W{CurrentLayer+1}(:,p);
+                    end
+                    %the previous output for the first hidden layer is the
+                    %input layer
+                    W{CurrentLayer}(:,p)=W{CurrentLayer}(:,p)+rate*Tdelta*double(train1(i,p))/256;
+                end
+                delta=[delta,Tdelta];
+            end
+        end
     end
 end
+
 end
 
